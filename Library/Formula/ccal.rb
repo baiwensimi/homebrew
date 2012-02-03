173b3dc4ae726c5154a28a58709263ab4625f7a3
Bai Wensimi <baiwensimi@gmail.com>
2012-02-03 17:01:28 +0800

    Add ccal 2.5.2
    	chinese calendar. command line tools. can also output pdf,xml,html

--- Library/Formula/ccal.rb
+++ Library/Formula/ccal.rb
@@ -5,20 +5,10 @@ class Ccal < Formula
   homepage 'http://ccal.chinesebay.com/ccal'
   md5 '93ba6c4af760bcef24aec7d483242b2b'
 
-  # depends_on 'cmake' => :build
-
   def install
     system "make"
 	system "make install"
 	system "make -e MANDIR=/usr/local/share/man install-man"
   end
 
-  def test
-    # This test will fail and we won't accept that! It's enough to just
-    # replace "false" with the main program this formula installs, but
-    # it'd be nice if you were more thorough. Test the test with
-    # `brew test ccal`. Remove this comment before submitting
-    # your pull request!
-    system "false"
-  end
 end
